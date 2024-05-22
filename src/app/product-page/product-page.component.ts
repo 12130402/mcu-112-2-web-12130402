933aee0bb1f8329d9d51b1c18318432d0c7662dd
12130402 <12130402@me.mcu.edu.tw>
2024-05-22 18:20:50 +0800

    feat(product-service): 變更產品資料新增、修改與刪除回傳型別

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -33,7 +33,7 @@ export class ProductPageComponent implements OnInit {
       createDate: new Date(),
       price: 10000,
     });
-    this.productService.add(product);
+    this.productService.add(product).subscribe();
   }
 
   onEdit(product: Product): void {
@@ -41,7 +41,7 @@ export class ProductPageComponent implements OnInit {
   }
 
   onRemove({ id }: Product): void {
-    this.productService.remove(id);
+    this.productService.remove(id).subscribe();
   }
 
   onView(product: Product): void {
